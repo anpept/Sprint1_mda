3dc770ae0a053eda9fad05db5eb0d61acb05cc2c
el_bomba <santi_ha97@hotmail.com>
2020-05-12 19:07:01 +0100

    visualizar, 12-05

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {LoadingController, ToastController, NavController} from '@ionic/angular';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {AngularFireAuth} from '@angular/fire/auth';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-user',
@@ -10,11 +12,55 @@ import {AngularFireAuth} from '@angular/fire/auth';
 })
 export class UserPage implements OnInit {
 
-  constructor(private firestore: AngularFirestore, public afAuth: AngularFireAuth, public navCtrl: NavController) { }
+  products: any;
+
+  constructor(
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private firestore: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    public navCtrl: NavController) {}
 
   ngOnInit() {
   }
 
+  ionViewWillEnter() {
+    this.getProducts();
+  }
+
+  async getProducts() {
+    // show loader
+    const loader = this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
+    (await loader).present();
+
+    try {
+      this.firestore.collection('products').snapshotChanges().subscribe(data => {
+        this.products = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            name: e.payload.doc.data()['name'],
+            type: e.payload.doc.data()['type'],
+            price: e.payload.doc.data()['price'],
+          };
+        });
+      });
+
+      // dismiss loader
+      (await loader).dismiss();
+    } catch (e) {
+      this.showToast(e);
+    }
+  }
+
+  showToast(message: string) {
+    this.toastCtrl.create({
+      message,
+      duration: 3000
+    }).then(toastData => toastData.present());
+  }
+  
   logout() {
     this.afAuth.signOut().then(() => {
       this.navCtrl.navigateRoot('login');
