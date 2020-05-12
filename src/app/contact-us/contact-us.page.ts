18b4de4a66ffba7d05e61cd3e804477ba1d8cdd4
Javier Alejandro Álvarez Mendoza <javi_67@hotmail.es>
2020-05-12 10:51:20 +0100

    commit del dia 12/05/2020

--- src/app/contact-us/contact-us.page.ts
+++ src/app/contact-us/contact-us.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../models/contact.module';
+import {LoadingController, NavController, ToastController} from '@ionic/angular';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {Observable} from 'rxjs';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +13,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.page.scss'],
 })
 export class ContactUsPage implements OnInit {
+contact = {} as Contact;
+  constructor(private toastCtrl: ToastController,
+              private loadingCtrl: LoadingController,
+              private navCtrl: NavController,
+              private afAuth: AngularFireAuth,
+              private firestore: AngularFirestore,
+              private storage: AngularFireStorage) { }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async createContact(contact: Contact) {
+    if (this.formValidation()) {
+      // show loader
+      const loader = this.loadingCtrl.create({
+        message: 'Please wait...'
+      });
+      (await loader).present();
+
+      try {
+        await this.firestore.collection('contact-us-section').add(contact);
+      } catch (e) {
+        this.showToast(e);
+      }
+
+      // dismiss loader
+      (await loader).dismiss();
+
+      // redirect to home page
+      this.navCtrl.navigateRoot('user');
+    }
   }
 
+  formValidation() {
+    if (!this.contact.name) {
+      this.showToast('Enter name');
+      return false;
+    }
+
+    if (!this.contact.email) {
+      this.showToast('Enter email');
+      return false;
+    }
+
+    if (!this.contact.phone) {
+      this.showToast('Enter phone');
+      return false;
+    }
+    if (!this.contact.message) {
+      this.showToast('Enter message');
+      return false;
+    }
+    return true;
+  }
+
+  showToast(message: string) {
+    this.toastCtrl.create({
+      message,
+      duration: 3000
+    }).then(toastData => toastData.present());
+  }
 }
