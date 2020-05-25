4ecaa3943036b0005a7f43bcbb777d4f8489213a
anpept <43388989+anpept@users.noreply.github.com>
2020-05-25 14:15:09 +0100

    Antonio Peñate - Pruebas iniciales para el pago

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from "firebase";
 import {AngularFireModule} from "@angular/fire";
+import {User} from "../models/user.model";
+import {LoadingController} from "@ionic/angular";
+import {AngularFirestore} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-checkout',
@@ -9,23 +12,46 @@ import {AngularFireModule} from "@angular/fire";
 })
 export class CheckoutPage implements OnInit {
 
+  id: any;
+  user = {} as User;
   cliente: any;
-  constructor(private afAuth: AngularFireModule) { }
+  constructor(private afAuth: AngularFireModule,
+              private loadingCtrl: LoadingController,
+              private firestore: AngularFirestore) { }
 
   ngOnInit() {
     this.cliente = firebase.auth().currentUser;
     if (this.cliente != null){
-      console.log(this.cliente);
+      this.getUserById(this.cliente.uid);
     }else{
-      console.log("Huje "+this.cliente);
+      console.log("No se han podido recuperar las credenciales");
     }
+
+  }
+
+  async getUserById(id: string){
+    const loader = this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
+    (await loader).present();
+
+    this.firestore.doc("users/" + id).valueChanges().subscribe(data =>{
+      this.user.email = data["email"];
+      this.user.password = data["password"];
+      this.user.name = data["name"];
+      this.user.surname = data["surname"];
+      this.user.type = data["type"];
+      this.user.category = data["category"];
+      this.user.other_data = data["other_data"];
+    });
+    (await loader).dismiss();
   }
 
   setUser() {
-    this.cliente = this.afAuth.auth().currentUser;
+    /*this.cliente = this.afAuth.auth().currentUser;
     if (this.cliente != null){
       console.log(this.cliente);
-    }
+    }*/
 
 
   }
